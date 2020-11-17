714972b7d083e1f5aa282ad8ac4425c0e7897680
h_hisano <h_hisano@hisanon.local>
2020-11-17 15:54:48 +0900

    Updates

---
+++ component---src-pages-page-2-js-59bc38f7ae12b18694da.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),l=t("Wbzz"),c=t("Bl7J");n.default=function(){return o.a.createElement(c.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-59bc38f7ae12b18694da.js.map
